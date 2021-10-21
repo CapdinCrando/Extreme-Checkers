0b02f29b0ba3a437eaa5a324012fb67f66956914
Tristan Jay <capdincrando@gmail.com>
2021-10-21 14:43:41 -0500

    Removed debug code

--- gameview.cpp
+++ gameview.cpp
@@ -75,7 +75,6 @@ void GameView::drawCheckers()
 	int step = BOARD_VIEW_SIZE/8;
 	int offset = step/8;
 	int scale = step*3/4;
-	static bool printed = false;
 	for(boardpos_t k = 0; k < SQUARE_COUNT; k++)
 	{
 		SquareState state = gameEngine.getSquareState(k);
@@ -83,10 +82,8 @@ void GameView::drawCheckers()
 		{
 			int y = k/4;
 			drawChecker(step*((k*2 + 1)%8 - (y%2)) + offset, step*(y) + offset, scale, scale, state);
-			//if(!printed) std::cout << (k*2 + 1)%9 << ", " << k/4 << std::endl;
 		}
 	}
-	printed = true;
 }
 
 void GameView::drawChecker(int x, int y, int w, int h, SquareState state)
@@ -95,8 +92,6 @@ void GameView::drawChecker(int x, int y, int w, int h, SquareState state)
 	QGraphicsEllipseItem* checker;
 	if(SQUARE_ISBLACK(state)) checker = scene->addEllipse(x, y, w, h, redPen, blackBrush);
 	else checker = scene->addEllipse(x, y, w, h, blackPen, redBrush);
-	//if(SQUARE_ISBLACK(state)) checker = scene->addEllipse(x, y, w, h, redPen, blackBrush);
-	//else checker = scene->addEllipse(x, y, w, h, blackPen, redBrush);
 	if(SQUARE_ISKING(state))
 	{
 		QGraphicsProxyWidget *proxyWidget = new QGraphicsProxyWidget(checker);
