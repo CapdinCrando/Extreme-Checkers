594bdbf1d815f9657054ba2e016930f78c4becb1
Tristan Jay <capdincrando@gmail.com>
2021-11-02 14:38:29 -0500

    Fix AIRandom

--- ai/airandom.cpp
+++ ai/airandom.cpp
@@ -17,32 +17,20 @@ Move AIRandom::getMove(GameBoard& board)
 		}
 	}
 
-	if(moves->empty())
+	Move move;
+	move.moveType = MOVE_INVALID;
+	if(!moves->empty())
 	{
-		Move m;
-		m.moveType = MOVE_INVALID;
-		return m;
+		move = moves->at(rand() % moves->size());
 	}
 
-	std::vector<result_t> results;
-
-	for(uint8_t i = 0; i < moves->size(); i++)
-	{
-		results.push_back(evalBlackMove(board, moves->at(i), 0));
-	}
-
-	// Pick result
-	auto iterator = std::max_element(std::begin(results), std::end(results));
-	size_t a = std::distance(results.begin(), iterator);
-	Move move = moves->at(a);
-
-	// Check for multijump
 	if(move.moveType == MOVE_JUMP_MULTI)
 	{
 		previousMultiJumpPos = move.newPos;
 	}
 	else previousMultiJumpPos = -1;
 
+	delete moves;
 	return move;
 }
 
