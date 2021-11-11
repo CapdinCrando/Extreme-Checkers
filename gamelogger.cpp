3be9a21da631d9ad3aa63715cf206d86e81d01c1
Tristan Jay <capdincrando@gmail.com>
2021-11-11 12:39:08 -0600

    Added functionality to logger

--- gamelogger.cpp
+++ gamelogger.cpp
@@ -11,7 +11,6 @@ GameLogger::~GameLogger()
 
 void GameLogger::openLogFile(GameSettings settings)
 {
-	// Close file if open
 	if(file.is_open()) file.close();
 
 	if(settings.enableLogging)
@@ -25,10 +24,44 @@ void GameLogger::openLogFile(GameSettings settings)
 		std::strftime(&logName[4], sizeof(logName), "%FT%H-%M-%S", &localTime);
 
 		file.open(logName, std::ios::out);
+
+		file << "============================ GAME INFORMATION ============================\n" <<
+				"File Name:," << logName << '\n' <<
+				"AI LEVEL:," << settings.aiLevel << '\n' <<
+				"==========================================================================\n\n" <<
+				"============================ MOVE INFORMATION ============================\n" <<
+				"Player,Old Position,New Position,Jump Position,Move Type,Move Duration (us)" << std::endl;
 	}
 }
 
 void GameLogger::logMessage(std::string message)
 {
 	if(file.is_open()) file << message << std::endl;
+}
+
+void GameLogger::logMove(Move move, bool isBlack, long long moveDurationMicro)
+{
+	if(file.is_open())
+	{
+		if(isBlack) file << "Black,";
+		else file << "Red,";
+		file << +move.oldPos << "," << +move.newPos << "," << +move.jumpPos << "," << +move.moveType << "," << +moveDurationMicro << std::endl;
+	}
+}
+
+void GameLogger::logGameOver(GameState gameState)
+{
+	if(file.is_open())
+	{
+		file << "\nGame Over State:," << gameState;
+
+		if(gameState == GAME_OVER_BLACK_WIN)
+			file << ",(Black Win)";
+		else if(gameState == GAME_OVER_RED_WIN)
+			file << ",(Red Win)";
+		else if(gameState == GAME_OVER_TIE)
+			file << ",(Tie)";
+
+		file.close();
+	}
 }
--- gamelogger.h
+++ gamelogger.h
@@ -18,6 +18,8 @@ public:
 public slots:
 	void openLogFile(GameSettings settings);
 	void logMessage(std::string message);
+	void logMove(Move move, bool isBlack, long long moveDurationMicro);
+	void logGameOver(GameState gameState);
 
 private:
 	std::ofstream file;
